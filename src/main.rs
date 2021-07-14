e4ac4a566b1e582bf0fb84a0d96bac11f0e29414
James Dunn <jamesallstar21@gmail.com>
2021-07-14 00:19:08 -0400

    moved logic out of main to own function

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ use std::collections::HashMap;
 use std::io::{self, Write};
 use tokio;
 
-
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 struct JiraBoardLocation {
@@ -52,6 +51,38 @@ fn get_user_input() -> String {
     input
 }
 
+async fn login() -> Result<(), reqwest::Error> {
+    print!("Site: ");
+    let site = get_user_input();
+
+    print!("username: ");
+    let username = get_user_input();
+
+    print!("password: ");
+    let password = get_user_input();
+
+    let merge = [username, ":".to_owned(), password].join("");
+    println!("merge {:?}", merge);
+    let encoded = encode(merge);
+    let client = reqwest::Client::new();
+    let res = client
+        .get(site + "/rest/agile/1.0/board")
+        .header(
+            reqwest::header::AUTHORIZATION,
+            "Basic ".to_owned() + &encoded,
+        )
+        .header(reqwest::header::CONTENT_TYPE, "application/json")
+        .send()
+        .await?
+        .text()
+        .await?;
+    let deserialize_jiraboard =
+        serde_json::from_str::<JiraBoard>(&res).expect("failed to deserialize json");
+
+    println!("{:?}", deserialize_jiraboard);
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let matches = App::new("Jiralang program")
@@ -61,35 +92,8 @@ async fn main() -> Result<(), reqwest::Error> {
         .get_matches();
 
     match matches.subcommand() {
-        ("login", Some(sub_m)) => {
-            print!("Site: ");
-            let site = get_user_input();
-
-            print!("username: ");
-            let username = get_user_input();
-
-            print!("password: ");
-            let password = get_user_input();
-
-            let merge = [username, ":".to_owned(), password].join("");
-            println!("merge {:?}", merge);
-            let encoded = encode(merge);
-            let client = reqwest::Client::new();
-            let res = client
-                .get(site + "/rest/agile/1.0/board")
-                .header(
-                    reqwest::header::AUTHORIZATION,
-                    "Basic ".to_owned() + &encoded,
-                )
-                .header(reqwest::header::CONTENT_TYPE, "application/json")
-                .send()
-                .await?
-                .text()
-                .await?;
-            let deserialize_jiraboard =
-                serde_json::from_str::<JiraBoard>(&res).expect("failed to deserialize json");
-
-            println!("{:?}", deserialize_jiraboard);
+        ("login", Some(_)) => {
+            login().await?;
         }
         (command, _) => unreachable!("invalid subcommand: {}", command),
     }
