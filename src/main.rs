cac2a2a9aa66eeb009e4b054d2521089159eb95e
James Dunn <jamesallstar21@gmail.com>
2021-07-15 23:27:09 -0400

    able to request for projects

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,8 @@ use ini::Ini;
 use reqwest;
 use serde::{Deserialize, Serialize};
 use serde_json;
-use std::collections::HashMap;
-use std::fs::File;
 use std::io::{self, Write};
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use tokio;
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -51,6 +49,65 @@ struct Config {
     password: String,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct ProjectAvatar {
+    #[serde(alias = "48x48")]
+    forty_eight: String,
+    #[serde(alias = "24x24")]
+    twenty_four: String,
+    #[serde(alias = "16x16")]
+    sixteen: String,
+    #[serde(alias = "32x32")]
+    thirty_two: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ProjectCategory {
+    #[serde(rename = "self")]
+    self_url: String,
+    id: String,
+    name: String,
+    description: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ProjectInsight {
+    total_issue_count: u32,
+    last_issue_update_time: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct ProjectValues {
+    expand: String,
+    #[serde(rename = "self")]
+    self_url: String,
+    id: String,
+    key: String,
+    name: String,
+    simplified: bool,
+    style: String,
+    is_private: bool,
+    entity_id: String,
+    uuid: String,
+    avatar_urls: ProjectAvatar,
+    project_category: Option<ProjectCategory>,
+    insight: Option<ProjectInsight>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Project {
+    #[serde(rename = "self")]
+    self_url: String,
+    next_page: Option<String>,
+    max_results: u32,
+    start_at: u32,
+    total: u32,
+    is_last: bool,
+    values: Vec<ProjectValues>,
+}
+
 fn get_user_input() -> String {
     let mut input = String::new();
     io::stdout().flush().expect("Failed to flush");
@@ -77,7 +134,7 @@ async fn config() -> Result<(), reqwest::Error> {
 }
 
 async fn get_jira_board() -> Result<(), reqwest::Error> {
-    let res = get_client_request("/rest/agile/1.0/board").await?; 
+    let res = get_client_request("/rest/agile/1.0/board").await?;
     let deserialized = serde_json::from_str::<JiraBoard>(&res).expect("failed to deserialize json");
 
     println!("{:?}", deserialized);
@@ -141,6 +198,16 @@ fn read_config() -> Result<Config, ini::Error> {
     })
 }
 
+async fn get_projects() -> Result<(), reqwest::Error> {
+    let res = get_client_request("/rest/api/3/project/search").await?;
+    println!("this is data {:?}", res);
+    let deserialize = serde_json::from_str::<Project>(&res).expect("Failed to deserialize data");
+
+    println!("{:?}", deserialize);
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let matches = App::new("Jiralang program")
@@ -148,11 +215,21 @@ async fn main() -> Result<(), reqwest::Error> {
         .author("James Dunn")
         .subcommand(SubCommand::with_name("config").about("setup config to login to jira"))
         .subcommand(SubCommand::with_name("board").about("Retrieve jira board"))
+        .subcommand(
+            SubCommand::with_name("projects")
+                .about("Jira projects")
+                .arg(Arg::with_name("get")),
+        )
         .get_matches();
 
     match matches.subcommand() {
         ("board", Some(_)) => get_jira_board().await?,
         ("config", Some(_)) => config().await?,
+        ("projects", Some(cmd)) => {
+            if let Some(_) = cmd.value_of("get") {
+                get_projects().await?;
+            }
+        }
         (command, _) => unreachable!("invalid subcommand: {}", command),
     }
     Ok(())
